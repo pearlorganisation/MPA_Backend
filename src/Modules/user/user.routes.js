235a05d63efe76c587e51fb320063594c92ca4a4
himanipanwarpearl <himanipanwar@pearlorganisation.com>
2026-02-28 18:32:12 +0530

    .

--- src/Modules/user/user.routes.js
+++ src/Modules/user/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+
 import {
   createUser,
   getAllEditors,
@@ -15,6 +15,11 @@ import {
 import { protect } from "../../Middlewares/auth.middleware.js";
 import { authorizeRoles } from "../../Middlewares/role.middleware.js";
 import passport from "passport";
+import User from "./user.model.js";
+
+ // adjust path if needed
+import generateToken from "../../utils/generateToken.js";
+
 
 const router = express.Router();
 
@@ -57,17 +62,55 @@ router.get(
   passport.authenticate("google", { scope: ["profile", "email"] })
 )
 
+// router.get(
+//   "/google/callback",
+//   passport.authenticate("google", { session: false }),
+//   (req, res) => {
+//     const email = req.user.email;
+     
+//      const isExistingUser =
+
+//     const token = jwt.sign(req.user, process.env.JWT_SECRET)
+
+//     // Redirect to frontend with token
+//     res.redirect(`${process.env.FRONTEND_URL}/login-success?token=${token}`)
+//   }
+// )
 
 router.get(
   "/google/callback",
   passport.authenticate("google", { session: false }),
-  (req, res) => {
-    const token = jwt.sign(req.user, process.env.JWT_SECRET)
+  async (req, res) => {
+    try {
+      const { email, name } = req.user;
 
-    // Redirect to frontend with token
-    res.redirect(`${process.env.FRONTEND_URL}/login-success?token=${token}`)
+      // 1️⃣ Check if user already exists
+      let user = await User.findOne({ email });
+
+      // 2️⃣ If not exist → create new user
+      if (!user) {
+        user = await User.create({
+          name,
+          email,
+          password: "google-oauth-user", // dummy password
+          isVerified: true, // Google users are already verified
+        });
+      }
+
+      // 3️⃣ Create token using user._id
+     const token = generateToken(user._id);
+
+      // 4️⃣ Redirect to frontend
+      res.redirect(
+        `${process.env.FRONTEND_URL}/login-success?token=${token}`
+      );
+      console.log("Google login successful", { email, name });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Google login failed" });
+    }
   }
-)
+);
 
 
 export default router;
