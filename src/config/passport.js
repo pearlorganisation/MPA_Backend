2473f15cd85ae35588c0886c8c04422d0233d047
Pearl Organisation™ <52455912+pearlorganisation@users.noreply.github.com>
2026-02-28 17:10:02 +0530

    Update Google OAuth callback URL based on environment

--- src/config/passport.js
+++ src/config/passport.js
@@ -13,7 +13,11 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/users/google/callback",
+      // callbackURL: "/api/v1/users/google/callback",
+      callbackURL:
+  process.env.NODE_ENV === "production"
+    ? "https://mpa-backend-og9r.onrender.com/api/v1/users/google/callback"
+    : "http://localhost:4040/api/v1/users/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = {
