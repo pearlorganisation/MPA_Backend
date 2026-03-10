41b44fdea109042679fd53c8fe3531a3a4cc1861
arjun-singh-sudo <arjun-singh@pearlorganisation.com>
2026-03-10 11:01:55 +0530

    changes

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,29 +1,26 @@
 import nodemailer from "nodemailer";
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // important
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
 const sendEmail = async (options) => {
-  console.log("EMAIL_USER:", process.env.EMAIL_USER);
-  console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
   console.log("Sending email to:", options.email);
 
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-
-    },
-  });
-
   const mailOptions = {
-    from: "Journal Portal <no-reply@journal.com>",
+    from: `Journal Portal <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
-    text: options.message,
     html: options.html,
   };
 
   await transporter.sendMail(mailOptions);
-  console.log("Email sent successfully ✅");
 };
 
 export default sendEmail;
