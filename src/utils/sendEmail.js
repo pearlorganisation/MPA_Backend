a33506ab15be20f7b5d8d6a262a21e610b212016
arjun-singh-sudo <arjun-singh@pearlorganisation.com>
2026-03-10 11:10:42 +0530

    changes

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,3 +1,42 @@
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "Gmail", 
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: "Journal Portal <no-reply@journal.com>",
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//     html: options.html,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// export default sendEmail;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
@@ -10,11 +49,13 @@ const transporter = nodemailer.createTransport({
   },
   tls: {
     rejectUnauthorized: false
-  }
+  },
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 20000,
 });
 
 const sendEmail = async (options) => {
-
   console.log("Sending email to:", options.email);
 
   const mailOptions = {
