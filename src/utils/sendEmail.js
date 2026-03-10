df19ca3e470f5dc98267201c9b89eef0db049aa6
arjun-singh-sudo <arjun-singh@pearlorganisation.com>
2026-03-10 10:38:27 +0530

    chnages

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,11 +1,16 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
+  console.log("EMAIL_USER:", process.env.EMAIL_USER);
+  console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+  console.log("Sending email to:", options.email);
+
   const transporter = nodemailer.createTransport({
-    service: "Gmail", 
+    service: "Gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
+
     },
   });
 
@@ -18,6 +23,7 @@ const sendEmail = async (options) => {
   };
 
   await transporter.sendMail(mailOptions);
+  console.log("Email sent successfully ✅");
 };
 
 export default sendEmail;
