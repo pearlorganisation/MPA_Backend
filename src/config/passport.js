9a8d06590271a5b9a1fc0e58825fb4385a600fcc
pearlorganisation <data-backup@pearlorganisation.com>
2026-04-20 10:18:12 +0530

    changes

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
@@ -13,11 +13,11 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    //   callbackURL: "/api/v1/users/google/callback",
-    callbackURL:
-  process.env.NODE_ENV === "production"
-    ? "https://mpa-backend-og9r.onrender.com/api/v1/users/google/callback"
-    : "http://localhost:4040/api/v1/users/google/callback",
+      //   callbackURL: "/api/v1/users/google/callback",
+      callbackURL:
+        process.env.NODE_ENV === "production"
+          ? "https://api.mparesearch.com/api/v1/users/google/callback"
+          : "http://localhost:4040/api/v1/users/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = {
